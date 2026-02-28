80b19a4af9a3cfc06ebfc9668ce125c0738fed8e
Bernsignal <porfily05@gmail.com>
2026-02-28 19:07:15 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,1 @@
-import telebot
-import os
-
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "🤖 Bern Signal Pro Bot la ap mache! 🚀")
-
-print("Bot la ap kouri...")
-bot.infinity_polling()
+bot.reply_to(message, "🚀 Ynerlio Signal Bot aktif! Pare pou voye siyal 🔥")
