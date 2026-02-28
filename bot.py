a2dd0e7fecac1fc7af7f0b6417ca5ff76ce76158
Bernsignal <porfily05@gmail.com>
2026-02-28 19:28:59 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-bot.reply_to(message, "🚀 Ynerlio Signal Bot aktif! Pare pou voye siyal 🔥")
+import telebot
+import os
+
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "🚀 Ynerlio Signal Bot aktif! Pare pou voye siyal 🔥")
+
+print("Bot la ap kouri...")
+bot.infinity_polling()
