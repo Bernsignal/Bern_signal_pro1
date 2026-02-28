76affd0d594ffd45beddbe77d0a33e936332d427
Bernsignal <porfily05@gmail.com>
2026-02-28 16:48:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
+
 import telebot
 import os
 
-# Li token lan depi environment variable sou Render
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
-
 bot = telebot.TeleBot(TOKEN)
 
-# Defini kòmand /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "🤖 Bern Signal Pro Bot la ap mache! 🚀")
